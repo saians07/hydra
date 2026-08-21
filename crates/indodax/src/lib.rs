2bfe7bb9e30bce2502cecd02d7799eeb9ce6cdd0
saians07 <suberlinsinaga@gmail.com>
2026-08-22 01:55:15 +0700

    Add private info and get_recv_window.

--- crates/indodax/src/lib.rs
+++ crates/indodax/src/lib.rs
@@ -1,10 +1,11 @@
 use argus::errors::CustomErr;
 use async_trait::async_trait;
 use hydra_core::{
-    balance::BalanceType,
+    market::crypto::CexMarket,
     utils::{RequestType, hmac_512},
 };
 use reqwest::{Client, Response, header::HeaderValue};
+use secrecy::{ExposeSecret, SecretString};
 
 pub struct Indodax {
     pub id: Box<str>,
@@ -13,16 +14,27 @@ pub struct Indodax {
     pub public_api_url: Box<str>,
     pub private_ws_url: Box<str>,
     pub public_ws_url: Box<str>,
-    pub balance: BalanceType,
+}
+
+impl Indodax {
+    pub fn new(id: Box<str>, name: Box<str>) -> Self {
+        Self {
+            id,
+            name,
+            private_api_url: "https://indodax.com/tapi".into(),
+            public_api_url: "https://indodax.com".into(),
+            private_ws_url: "".into(),
+            public_ws_url: "".into(),
+        }
+    }
 }
 
 #[async_trait]
-impl hydra_core::market::CryptoCexMarket for Indodax {
-    /// Build payload from any kind of srtucture data to match
-    /// the API query.
-    async fn build_payload<T: serde::Serialize + Send + Sync + 'static>(
+impl CexMarket for Indodax {
+    /// Serialize any struct to build the payload data.
+    async fn build_payload<P: serde::Serialize + Send + Sync + 'static>(
         &self,
-        payload: T,
+        payload: P,
     ) -> Result<Box<str>, CustomErr> {
         match serde_qs::to_string(&payload) {
             Ok(payload) => return Ok(Box::from(payload)),
@@ -30,8 +42,12 @@ impl hydra_core::market::CryptoCexMarket for Indodax {
         }
     }
 
-    async fn sign_payload(&self, payload: &str, key: &str) -> Result<Box<str>, CustomErr> {
-        let result = hmac_512(payload, key)?;
+    async fn sign_payload(
+        &self,
+        payload: &str,
+        sign_key: SecretString,
+    ) -> Result<Box<str>, CustomErr> {
+        let result = hmac_512(payload, sign_key.expose_secret())?;
 
         Ok(Box::from(result))
     }
@@ -40,7 +56,7 @@ impl hydra_core::market::CryptoCexMarket for Indodax {
         &self,
         body: &str,
         url: &str,
-        sign_key: Option<&str>,
+        sign_key: Option<SecretString>,
         request_type: RequestType,
         client: Client,
     ) -> Result<Response, CustomErr> {
@@ -63,11 +79,14 @@ impl hydra_core::market::CryptoCexMarket for Indodax {
                         source: None,
                     });
                 };
-                let signed_body = self.sign_payload(body, sign_key).await?;
+                let signed_body = self.sign_payload(body, sign_key.clone()).await?;
                 let result = client
                     .post(url)
                     .header("Content-Type", "application/x-www-form-urlencoded")
-                    .header("Key", HeaderValue::from_str(sign_key).unwrap())
+                    .header(
+                        "Key",
+                        HeaderValue::from_str(&sign_key.clone().expose_secret()).unwrap(),
+                    )
                     .header("Sign", HeaderValue::from_str(&signed_body).unwrap())
                     .send()
                     .await
@@ -78,5 +97,42 @@ impl hydra_core::market::CryptoCexMarket for Indodax {
                 Ok(result)
             }
         }
+    }
+
+    async fn get_recv_window(&self) -> Result<(i64, i64), CustomErr> {
+        let now = chrono::Local::now().timestamp_millis();
+
+        // this will be deduced with how long the request is valid for
+        let recv_window = now - 4000;
+
+        Ok((now, recv_window))
+    }
+}
+
+// this is the specific implementation of Indodax endpoint.
+impl Indodax {
+    /// Private getInfo endpoint of Indondax
+    pub async fn priv_get_info(
+        &self,
+        sign_key: SecretString,
+        client: Client,
+    ) -> Result<Response, CustomErr> {
+        let (now, recv_window) = self.get_recv_window().await?;
+        let body = format!(
+            "method=getInfo&timestamp={:?}&recvWindow={:?}",
+            now, recv_window
+        );
+
+        let result = self
+            .send_request(
+                &body,
+                &self.private_api_url,
+                Some(sign_key),
+                RequestType::POST,
+                client,
+            )
+            .await?;
+
+        Ok(result)
     }
 }
