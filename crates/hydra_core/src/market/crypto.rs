a84a80b533a99369161a2b256cf3c89fb3ba5d85
saians07 <suberlinsinaga@gmail.com>
2026-08-22 01:52:51 +0700

    Move market.rs into a folder since it can be crypto and non crypto

--- crates/hydra_core/src/market/crypto.rs
+++ crates/hydra_core/src/market/crypto.rs
@@ -1,22 +1,28 @@
 use argus::errors::CustomErr;
 use async_trait::async_trait;
 use reqwest::{Client, Response};
+use secrecy::SecretString;
 
 use crate::utils::RequestType;
 
 #[async_trait]
-pub trait CryptoCexMarket {
+pub trait CexMarket {
     async fn build_payload<T: serde::Serialize + Send + Sync + 'static>(
         &self,
         payload: T,
     ) -> Result<Box<str>, CustomErr>;
-    async fn sign_payload(&self, payload: &str, key: &str) -> Result<Box<str>, CustomErr>;
+    async fn sign_payload(
+        &self,
+        payload: &str,
+        sign_key: SecretString,
+    ) -> Result<Box<str>, CustomErr>;
     async fn send_request(
         &self,
         body: &str,
         url: &str,
-        sign_key: Option<&str>,
+        sign_key: Option<SecretString>,
         request_type: RequestType,
         client: Client,
     ) -> Result<Response, CustomErr>;
+    async fn get_recv_window(&self) -> Result<(i64, i64), CustomErr>;
 }
