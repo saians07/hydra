25b13f3994871971877addfe00f9b065b6ba04ec
saians07 <suberlinsinaga@gmail.com>
2026-08-27 17:33:42 +0700

    Add dynamic balance type that can be used to update the balance for each token.

--- crates/hydra_core/src/balance.rs
+++ crates/hydra_core/src/balance.rs
@@ -1,3 +1,6 @@
+use std::{collections::HashMap, fmt::Display, str::FromStr};
+
+use argus::errors::ArgusErr;
 use async_trait::async_trait;
 use rust_decimal::Decimal;
 
@@ -5,7 +8,43 @@ use rust_decimal::Decimal;
 pub struct Balance {
     pub available: Decimal,
     pub locked: Decimal,
-    pub freeze: Decimal,
+    pub frozen: Decimal,
+}
+
+impl Display for Balance {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "available: {}\n", self.available)?;
+        write!(f, "locked: {}\n", self.locked)?;
+        write!(f, "frozen: {}\n", self.frozen)
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct BalanceType(pub HashMap<String, Balance>);
+
+impl FromStr for BalanceType {
+    type Err = ArgusErr;
+    /// Automatically convert any string balance into hashmap
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let lower_case = s.to_owned().to_lowercase();
+
+        let mut map = HashMap::new();
+        map.insert(lower_case, Balance::default());
+
+        Ok(BalanceType(map))
+    }
+}
+
+impl Display for BalanceType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Wallet Balance: {{\n")?;
+
+        for (token, balance) in &self.0 {
+            write!(f, "  {}: \n{},\n", token, balance)?;
+        }
+
+        write!(f, "}}")
+    }
 }
 
 #[async_trait]
